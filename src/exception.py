03c33b3fa951e3169df533fd010ac496a4d06706
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-05-05 14:43:52 +0530

    nfothin

---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys
+from src.logger.logger import logging
+
+def error_message_details(error,error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    
+    error_message = "Error occured in python Script Name [{0}] at Line Number [{1}] with Error Message [{2}]".format(filename, exc_tb.tb_lineno, error)
+    
+    return error_message
+
+
+class CustomException(Exception):
+    
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,error_details=error_details)
+        
+        def __str__(self):
+            return self.error_message
